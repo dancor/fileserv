94f239a76df75a4d09612f76f5d79334a7f6be26
dan corson <danl@coky.(none)>
2008-08-25 19:58:51 -0700

    initial commit for fileserv

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,47 @@
-module Main where
+import Control.Monad.Trans
+import Data.List
+import Data.Maybe
+import HAppS.Server
+import System.Directory
+import System.Environment
+import System.FilePath
+import System.Console.GetOpt
+import Text.XHtml hiding (dir)
+import qualified Data.Map as Map
 
-main :: IO ()
+data Opts = Opts {
+  optPort :: Int
+  }
+
+defOpts = Opts {
+  optPort = 80
+  }
+
+progOpts = [
+  Option ['p'] ["port"]
+    (ReqArg (\ port opts -> opts {optPort = read port}) "PORT")
+    "PORT to serve on"
+  ]
+
+dirify :: FilePath -> [String] -> Html
+dirify dir fs = concatHtml $ map (\d -> toHtml
+  (HotLink (joinPath ["/", dir, d]) (toHtml d) []) +++ br) $
+  sort fs
+
+myMimeTypes = Map.insert "mem" "text/plain; charset=utf-8" mimeTypes
+
 main = do
-  putStrLn "hi"
+  --print myMimeTypes
+  args <- getArgs
+  let header = "lol"
+  (opts, []) <- case getOpt Permute progOpts args of
+    (o, n, []) -> return (foldl (flip id) defOpts o, n)
+    (_, _, errs) -> 
+      ioError (userError (concat errs ++ usageInfo header progOpts))
+  simpleHTTP nullConf {port=(optPort opts)} [
+    dir "req" [withRequest $ ok . toResponse . show],
+    withRequest $ fileServe' "." fdir myMimeTypes] where
+      fdir _mime _rq fp = do
+        contents <- liftIO $ getDirectoryContents fp
+        return $ toResponse $ dirify fp $ filter (`notElem` ["."] ++
+          (if fp == "." then [".."] else [])) $ contents
